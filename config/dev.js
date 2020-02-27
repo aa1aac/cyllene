ef616133149206af89620626ae609a248e47529e
Aayush Acharya <ach.aayush.111@gmail.com>
2020-02-27 22:18:41 +0545

    del

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGO_URI:
-    "mongodb+srv://aa11:w1rePlfF2yVRBJHq@cluster0-abwod.mongodb.net/test?retryWrites=true&w=majority",
-  SECRET:
-    "fdshakjhkjdshfkjdshafkjhsdkjhdsklasadlkjhdsakjfhkjdhsfjkhdfkjhdskjufhyiudhygiurtdhfsd"
-};
